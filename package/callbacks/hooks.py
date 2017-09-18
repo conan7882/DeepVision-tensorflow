f3c9720b6465dc666b9c4eba7e9019552c3d66ea
Qian Ge <qge2@ncsu.edu>
2017-09-17 23:15:12 -0400

    building inference

--- package/callbacks/hooks.py
+++ package/callbacks/hooks.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 from .base import Callback
 
-__all__ = ['Callback2Hook']
+__all__ = ['Callback2Hook', 'Infer2Hook']
 
 def assert_type(v, tp):
     assert isinstance(v, tp), "Expect " + str(tp) + ", but " + str(v.__class__) + " is given!"
@@ -18,6 +18,19 @@ class Callback2Hook(tf.train.SessionRunHook):
 
     def after_run(self, rct, val):
         self.cb.after_run(rct, val)
+
+class Infer2Hook(tf.train.SessionRunHook):
+	def __init__(self, infer_term):
+		# to be modified 
+		# add type assert
+		self.infer_term = infer_term
+
+	def before_run(self, rct):
+		return tf.train.SessionRunArgs(fetches = self.infer_term)
+
+	def after_run(self, rct, val):
+		print(val)
+
 
     
 
--- package/callbacks/inference.py
+++ package/callbacks/inference.py
@@ -1,4 +1,5 @@
-
+# File: inference.py
+# Author: Qian Ge <geqian1001@gmail.com>
 
 import scipy.misc
 import os
@@ -9,43 +10,77 @@ import tensorflow as tf
 
 from .base import Callback
 from ..dataflow.base import DataFlow
+from .hooks import Callback2Hook, Infer2Hook
+from ..utils.sesscreate import ReuseSessionCreator
 
-__all__ = ['Inference']
+__all__ = ['InferenceBase', 'FeedInference']
 
 def assert_type(v, tp):
     assert isinstance(v, tp), "Expect " + str(tp) + ", but " + str(v.__class__) + " is given!"
 
-class Inference(Callback):
+class InferenceBase(Callback):
     """ base class for Inference """
+
+    def _setup_graph(self):
+        self.model = self.trainer.model
+        self.sess = self.trainer.sess
+        self.setup_inference()
+        
+    def setup_inference(self):
+        self._setup_inference()
+        self._create_infer_sess()
+        self.Inference_list = self.model.get_inference_list()
+
+    def _setup_inference(self):
+        """ setup extra hooks for inference """
+        self.cbs = []
+
+    def get_infer_hooks(self):
+        if not isinstance(self.cbs, list):
+            self.cbs = [self.cbs]
+        infer_hooks = Infer2Hook(self.Inference_list)
+        infer_hooks.append([Callback2Hook(cb) for cb in self.cbs])
+        return infer_hooks
+        
+    def _create_infer_sess(self):
+        infer_hooks = self.get_infer_hooks()
+        self.hooked_sess = tf.train.MonitoredSession(
+            session_creator = ReuseSessionCreator(self.sess), hooks = infer_hooks)
+
+    def _trigger_step(self):
+        self.inference_step()
+
+        # assert len(cur_batch) == len(self.placeholders), \
+        # "[FeedInput] lenght of input {} is not equal to length of placeholders {}"\
+        # .format(len(cur_batch), len(self.placeholders))
+
+        # feed = dict(zip(self.placeholders, cur_batch))
+        # self.trainer.model.set_is_training(False)
+        # accuracy = self.Inference_list.eval(feed_dict = feed)
+        
+        # print(accuracy)
+
+    def inference_step(self):
+        self.model.set_is_training(False)
+        self._inference_step()
+
+    def _inference_step(self):
+        self.hooked_sess.run()
+        
+
+class FeedInference(InferenceBase):
     def __init__(self, dataflow):
         assert_type(dataflow, DataFlow)
         self.dataflow = dataflow
 
-        # self.test_tensor = test_tensor
+    def _setup_inference(self):
+        placeholders = self.model.get_placeholder()
+        self.cbs = FeedInput(self.dataflow, placeholders)
 
-    # def setup_graph(self, trainer):
-    #     self.trainer = trainer
-    #     self._setup_graph()
-
-
-    def _setup_graph(self):
-        self.placeholders = self.trainer.model.get_placeholder()
-        self.Inference_list = self.trainer.model.get_inference_list()
-        # cbs = FeedInput(self.dataflow, self.trainer.model.get_placeholder())
+    # def _inference_step(self):
+    #     cur_batch = self.dataflow.next_batch()
 
 
-    def _trigger_step(self):
-        cur_batch = self.dataflow.next_batch()
-        
-        assert len(cur_batch) == len(self.placeholders), \
-        "[FeedInput] lenght of input {} is not equal to length of placeholders {}"\
-        .format(len(cur_batch), len(self.placeholders))
-
-        feed = dict(zip(self.placeholders, cur_batch))
-        self.trainer.model.set_is_training(False)
-        accuracy = self.Inference_list.eval(feed_dict = feed)
-        
-        print(accuracy)
 
 
 
