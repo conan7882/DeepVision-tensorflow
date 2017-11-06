0389cd7839fc394e93f586706077d9cea6067969
Qian Ge <qge2@ncsu.edu>
2017-11-06 13:19:27 -0500

    update setup

--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ setup(name='tensorcv',
       url='https://github.com/conan7882/DeepVision-tensorflow',
       author='Qian Ge',
       author_email='geqian1001@gmail.com',
-      packages=['tensorcv', 'tensorcv.utils'],
+      packages=['tensorcv', 'tensorcv.utils', 'tensorcv.algorithms', 'tensorcv.callbacks',
+                'tensorcv.data', , 'tensorcv.dataflow', 'tensorcv.dataflow.dataset',
+                'tensorcv.models', , 'tensorcv.predicts', 'tensorcv.train'],
       zip_safe=False)
