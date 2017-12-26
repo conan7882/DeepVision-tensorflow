3e468933ece0e01246bb64ad0975594731ba063a
Qian Ge <qge2@ncsu.edu>
2017-12-26 14:27:56 -0500

    edit doc for data prepropossing

--- tensorcv/dataflow/preprocess.py
+++ tensorcv/dataflow/preprocess.py
@@ -6,8 +6,20 @@
 import numpy as np
 from scipy import misc
 
+from .common import get_shape2D
+
 
 def image_fliplr(image):
+    """ Generate mirror image
+
+    Args:
+        image (np.array): a 2-D image of shape
+            [height, width] or a 3-D image of shape
+            [height, width, channels].
+
+    Returns:
+        mirror version of original image.
+    """
     return np.fliplr(image)
 
 
@@ -17,10 +29,13 @@ def resize_image_with_smallest_side(image, small_size):
     keep the original aspect ratio.
 
     Args:
-        image (np.array): 2-D image of shape
-            [height, width] or 3-D image of shape
+        image (np.array): a 2-D image of shape
+            [height, width] or a 3-D image of shape
             [height, width, channels].
         small_size (int): A 1-D int. The smallest side of resize image.
+
+    Returns:
+        rescaled image
     """
     im_shape = image.shape
     shape_dim = len(im_shape)
@@ -45,6 +60,18 @@ def resize_image_with_smallest_side(image, small_size):
 
 
 def random_crop_to_size(image, crop_size):
+    """ Rondomly crop an image into crop_size
+
+    Args:
+        image (np.array): a 2-D image of shape
+            [height, width] or a 3-D image of shape
+            [height, width, channels].
+            The size has to be larger than cropped image.
+        crop_size (int or length 2 list): The image size after cropped.
+
+    Returns:
+        cropped image
+    """
     crop_size = get_shape2D(crop_size)
     im_shape = image.shape
     shape_dim = len(im_shape)
@@ -53,14 +80,27 @@ def random_crop_to_size(image, crop_size):
     height = im_shape[0]
     width = im_shape[1]
     assert height >= crop_size[0] and width >= crop_size[1],\
-    'Image must be larger than crop size! {}'.format(im_shape)
+        'Image must be larger than crop size! {}'.format(im_shape)
 
     s_h = int(np.floor((height - crop_size[0] + 1) * np.random.rand()))
     s_w = int(np.floor((width - crop_size[1] + 1) * np.random.rand()))
 
     return image[s_h:s_h + crop_size[0], s_w:s_w + crop_size[1]]
 
+
 def four_connor_crop(image, crop_size):
+    """ Crop an image into crop_size with four corner crops
+
+    Args:
+        image (np.array): a 2-D image of shape
+            [height, width] or a 3-D image of shape
+            [height, width, channels].
+            The size has to be larger than cropped image.
+        crop_size (int or length 2 list): The image size after cropped.
+
+    Returns:
+        four cropped images
+    """
     crop_size = get_shape2D(crop_size)
     im_shape = image.shape
     shape_dim = len(im_shape)
@@ -68,7 +108,7 @@ def four_connor_crop(image, crop_size):
     height = im_shape[0]
     width = im_shape[1]
     assert height >= crop_size[0] and width >= crop_size[1],\
-    'Image must be larger than crop size! {}'.format(im_shape)
+        'Image must be larger than crop size! {}'.format(im_shape)
 
     crop_im = []
     crop_im.append(image[: crop_size[0], : crop_size[1]])
@@ -80,6 +120,18 @@ def four_connor_crop(image, crop_size):
 
 
 def center_crop(image, crop_size):
+    """ Center crop an image into crop_size
+
+    Args:
+        image (np.array): a 2-D image of shape
+            [height, width] or a 3-D image of shape
+            [height, width, channels].
+            The size has to be larger than cropped image.
+        crop_size (int or length 2 list): The image size after cropped.
+
+    Returns:
+        cropped images
+    """
     crop_size = get_shape2D(crop_size)
     im_shape = image.shape
     shape_dim = len(im_shape)
@@ -87,27 +139,42 @@ def center_crop(image, crop_size):
     height = im_shape[0]
     width = im_shape[1]
     assert height >= crop_size[0] and width >= crop_size[1],\
-    'Image must be larger than crop size! {}'.format(im_shape)
+        'Image must be larger than crop size! {}'.format(im_shape)
 
-    return image[(height - crop_size[0])//2:(height + crop_size[0])//2, 
+    return image[(height - crop_size[0])//2:(height + crop_size[0])//2,
                  (width - crop_size[1])//2:(width + crop_size[1])//2]
 
 
-def get_shape2D(in_val):
-    """
-    Return a 2D shape 
+def random_mirror_resize_crop(image, crop_size, scale_range, mirror_rate=0.5):
+    """ Ramdomly rescale, crop and image.
 
     Args:
-        in_val (int or list with length 2) 
+        image (np.array): a 2-D image of shape
+            [height, width] or a 3-D image of shape
+            [height, width, channels].
+        crop_size (int or length 2 list): The image size after cropped.
+        scale_range (list of int with length 2): The range of scale.
+        mirror_rate (float): The probability of mirror image.
+            Must within the range [0, 1]
 
     Returns:
-        list with length 2
+        cropped and rescaled images
     """
-    if in_val is None:
-        return None
-    if isinstance(in_val, int):
-        return [in_val, in_val]
-    if isinstance(in_val, list):
-        assert len(in_val) == 2
-        return in_val
-    raise RuntimeError('Illegal shape: {}'.format(in_val))
+    im_shape = image.shape
+    shape_dim = len(im_shape)
+
+    assert mirror_rate >= 0 and mirror_rate <= 1,\
+        'mirror rate must be in range of [0, 1]!'
+    assert shape_dim <= 3 and shape_dim >= 2,\
+        'Wrong format of image!Shape is {}'.format(im_shape)
+
+    small_size = int(np.random.rand() * (max(scale_range) - min(scale_range))
+                     + min(scale_range))
+    image = resize_image_with_smallest_side(image, small_size)
+
+    image = random_crop_to_size(image, crop_size)
+
+    if np.random.rand() >= mirror_rate:
+        image = image_fliplr(image)
+
+    return image
